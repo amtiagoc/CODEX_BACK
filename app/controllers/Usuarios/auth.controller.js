09da5876d07edda1a2eb80ff815e0321eba3e722
Santiago Cano <cano2030@gmail.com>
2021-05-05 19:20:01 -0500

    clase para auth token y llamada en router

---
+++ app/controllers/Usuarios/auth.controller.js
@@ -0,0 +1,74 @@
+const _jwt = require("../../services/jwt.service");
+const PostgresService = require("../../services/postgres.service");
+const _pg = new PostgresService();
+
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @returns
+ */
+const getUserLogin = async (req, res) => {
+  try {
+    let user = req.body;
+    let sql = `select name, rol, id from users WHERE id='${user.id}' and password=md5('${user.password}') limit 1`;
+    let result = await _pg.executeSql(sql);
+    let user_logged = result.rows[0];
+    console.log(user_logged);
+    let token = user_logged ? _jwt.sign(user_logged) : null;
+    return res.send({
+      ok: user_logged ? true : false,
+      message: user_logged
+        ? `Bienvenido ${user_logged.name}`
+        : "Usuario no encontrado, verificar identificación y/o contraseña.",
+      content: { token, name: user_logged.name, rol: user_logged.rol },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      ok: false,
+      message: "Ha ocurrido un error consultando el usuario.",
+      content: error,
+    });
+  }
+};
+
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @returns
+ */
+const verifyToken = (req, res) => {
+  try {
+    let token = req.headers.token;
+    let user = _jwt.verify(token);
+    return res.status(200).send({
+      ok: true,
+      message: "Token verificado",
+      content: user,
+    });
+  } catch (error) {
+    return res.status(401).send({
+      ok: false,
+      message: "Error verificando el token.",
+      content: error,
+    });
+  }
+};
+
+const verifyTokenMiddleware = (req, res, next) => {
+  try {
+    let token = req.headers.token;
+    let user = _jwt.verify(token);
+    next();
+  } catch (error) {
+    return res.send({
+      ok: false,
+      message: "Middleware - Error verificando el token.",
+      content: error,
+    });
+  }
+};
+
+module.exports = { getUserLogin, verifyToken, verifyTokenMiddleware };
--- app/routers/index.js
+++ app/routers/index.js
@@ -1,11 +1,42 @@
 const express = require('express');
-
 const router = express.Router();
-const _usuariosController = require('../controllers/Usuarios/Usuarios.controller')
+const _usuariosController = require('../controllers/Usuarios/Usuarios.controller');
+const _entradasController = require("../controllers/Entradas/Entradas.Controller");
+const _examenesController = require("../controllers/Examenes/Examenes.controller");
+const _authController = require("../controllers/Usuarios/auth.controller");
 
+
+// RUTAS PUBLICAS
+// Rutas no necesitan un token
+router.post("/login", _authController.getUserLogin);
+
+//REGISTRO DEL MIDDLEWARE
+router.use([_authController.verifyTokenMiddleware]);
+// RUTAS PRIVADAS
 router
-    .get('/usuarios', _usuariosController.getUsuarios)
-    .get('/usuario', _usuariosController.getUsuario)
-    .post('/', _usuariosController.createUsuario);
+  // Descrifrar y verificar token
+  .get("/verify", _authController.verifyToken)
+  //CRUD USUARIOS
+  .get('/usuarios', _usuariosController.getUsuarios)
+  .get('/usuario', _usuariosController.getUsuario)
+  .post('/', _usuariosController.createUsuario);
 
-module.exports = router;
+  //CRUD ENTRADAS
+
+
+  //CRUD EXAMENES
+
+module.exports = router;    
+
+
+
+
+
+
+
+
+
+
+
+  //CRUD ENTRADAS
+
